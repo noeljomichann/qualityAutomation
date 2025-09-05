fd9b8b1621ce0f3935bfcc0c522af8a0073426a2
noeljomichann <noel.jomichan@oneture.com>
2025-09-05 18:35:16 +0530

    Updated VerificationScreen.tsx

--- src/components/VerificationScreen.tsx
+++ src/components/VerificationScreen.tsx
@@ -32,7 +32,7 @@ export const VerificationScreen: React.FC<VerificationScreenProps> = ({
       setLoading(true);
       setError(null);
       
-      const baseUrl = "https://6205cded8be9.ngrok-free.app";
+      const baseUrl = "https://cd3a4ea87360.ngrok-free.app";
       const endpoint = getEndpointForCard(title);
       const fullUrl = `${baseUrl}/${endpoint}`;
 
