9b49bd64d106b2a6e2eaf526d226029ce825aeb1
noeljomichann <noel.jomichan@oneture.com>
2025-09-05 17:57:52 +0530

    Updated VerificationScreen.tsx

--- src/components/VerificationScreen.tsx
+++ src/components/VerificationScreen.tsx
@@ -32,7 +32,7 @@ export const VerificationScreen: React.FC<VerificationScreenProps> = ({
       setLoading(true);
       setError(null);
       
-      const baseUrl = "https://962b83dd2a63.ngrok-free.app";
+      const baseUrl = "https://6205cded8be9.ngrok-free.app";
       const endpoint = getEndpointForCard(title);
       
       // Convert base64 image to blob
